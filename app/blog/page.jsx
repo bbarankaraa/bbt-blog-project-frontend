6add64721235a59a581922bcd588470fcd89159a
Arda Baran Kara <141037336+bbarankaraa@users.noreply.github.com>
2025-12-23 15:35:34 +0300

    Simplify BlogPage component
    Removed blog fetching logic and simplified BlogPage component.

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,13 +1,7 @@
 import BlogClient from "@/components/BlogClient";
-import { postService } from "@/services/postService";
 
-const BlogPage = async () => {
-  const blogs = await postService.getAllPosts();
-
-  const firstBlog = blogs && blogs[blogs.length - 1];
-  const otherBlogs = blogs?.length > 0 && blogs.slice(0, -1);
-
-  return <BlogClient firstBlog={firstBlog} otherBlogs={otherBlogs} />;
+const BlogPage = () => {
+  return <BlogClient />;
 };
 
 export default BlogPage;
